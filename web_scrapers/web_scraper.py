32a10ea34208021adbf13849409f7aea37c5322e
roba7o <rhm14148@gmail.com>
2024-12-08 21:14:04 +0000

    venv initialisation

---
+++ web_scrapers/web_scraper.py
@@ -0,0 +1,22 @@
+"""
+Initial web scraper, will reformat this and create python 
+package but initially just want to get the scraping script working for the following
+News sites:
+
+Le monde
+France24
+Figaro
+francetvinfo
+Libération
+"""
+
+
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import os
+
+
+
+
