bc1ee08301e937dfc231b6984cc4cd1528b1c75a
roba7o <rhm14148@gmail.com>
2025-02-04 08:40:37 +0000

    scraper full top 8 articles on slatefr

--- article_scrapers/main.py
+++ article_scrapers/main.py
@@ -11,32 +11,39 @@ keeping it simple at start, no need for relational db
 """
 
 from parsers.slate_fr_parser import SlateFrArticleParser
-# from scrapers.slate_fr_scraper import SlateFrScraper
+from scrapers.slate_fr_scraper import SlateFrURLScraper
 from utils.csv_writer import write_to_csv
 
 
 def main():
 
     slate_parser = SlateFrArticleParser()
-    # slate_scraper = SlateFrScraper()    #Eventually get this to grap all releveant urls
+    slate_scraper = SlateFrURLScraper()  #Eventually get this to grap all releveant urls
 
-    # TODO Grabbing slate urls
-    # slate_urls = slate_scraper.get_article_urls()
 
-    test_slate_urls = [
+    # Grabbing slate urls
+    live_scraper = True # TODO change for live version
+    if live_scraper:
+        slate_urls = slate_scraper.get_article_urls()
+    else:
+        test_slate_urls = [
         "https://www.slate.fr/monde/regle-baillon-mondial-trump-entraver-acces-avortement-mexico-city-policy-anti-ivg-dangers-mort-femmes-deces-grossesse",
         "https://www.slate.fr/monde/canada-quelque-chose-mysterieux-tue-grands-requins-blancs-cerveau-inflammation-maladie-autopsie-deces-mort-scientifiques"
-    ]
+        ]
 
-    test_local_files = [
-        "test_slate_article.html",
-        "test_slate_article2.html"
-    ]
+        test_local_files = [
+            "test_slate_article.html",
+            "test_slate_article2.html"
+        ]
 
-    live = True # TODO change for live version
+    
 
-    if live:
-        soups_url_pairs = [(slate_parser.get_soup_from_url(url), url) for url in test_slate_urls]
+    
+
+    live_parser = True # TODO change for live version
+
+    if live_parser:
+        soups_url_pairs = [(slate_parser.get_soup_from_url(url), url) for url in slate_urls]
     else:
         soups_url_pairs = [(slate_parser.get_soup_from_localfile(file), file) for file in test_local_files]
 
