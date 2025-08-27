e08a50e8585f467bd85c13b7c67a3bc5b9f81d76
roba7o <rhm14148@gmail.com>
2025-08-27 07:58:40 +0100

    innit files refactored afther pakcage moved

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -9,7 +9,7 @@ Note: Text processing and filtering is now handled by dbt/SQL.
 Stopwords and junk word configurations are in french_flashcards/config/ as JSON files.
 """
 
-from .environment import DEBUG, TEST_MODE, DATABASE_CONFIG, get_news_data_schema
+from .environment import DEBUG, TEST_MODE, DATABASE_CONFIG, get_news_data_schema, CONCURRENT_FETCHERS, FETCH_TIMEOUT
 from .site_configs import SCRAPER_CONFIGS
 
 __all__ = [
@@ -17,5 +17,7 @@ __all__ = [
     "TEST_MODE",
     "DATABASE_CONFIG",
     "get_news_data_schema",
+    "CONCURRENT_FETCHERS",
+    "FETCH_TIMEOUT",
     "SCRAPER_CONFIGS",
 ]
