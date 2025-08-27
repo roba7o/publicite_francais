8c58b9acb7bf62962a67bdbee9803ef98e3b3471
roba7o <rhm14148@gmail.com>
2025-08-27 15:46:38 +0100

    __init__.py healthy imports

--- src/core/components/soup_validators/__init__.py
+++ src/core/components/soup_validators/__init__.py
@@ -3,3 +3,17 @@ Soup Validators package for French news scraper.
 
 Contains soup validators for different news sources.
 """
+
+from .base_soup_validator import BaseSoupValidator
+from .france_info_soup_validator import FranceInfoSoupValidator
+from .ladepeche_fr_soup_validator import ladepechefrSoupValidator
+from .slate_fr_soup_validator import SlateFrSoupValidator
+from .tf1_info_soup_validator import tf1infoSoupValidator
+
+__all__ = [
+    "BaseSoupValidator",
+    "FranceInfoSoupValidator",
+    "ladepechefrSoupValidator",
+    "SlateFrSoupValidator",
+    "tf1infoSoupValidator",
+]
--- src/core/components/url_collectors/__init__.py
+++ src/core/components/url_collectors/__init__.py
@@ -3,3 +3,17 @@ URL Collectors package for French news scraper.
 
 Contains URL collectors for different news sources.
 """
+
+from .base_url_collector import BaseUrlCollector
+from .france_info_url_collector import FranceInfoUrlCollector
+from .ladepeche_fr_url_collector import LadepecheFrUrlCollector
+from .slate_fr_url_collector import SlateFrUrlCollector
+from .tf1_info_url_collector import TF1InfoUrlCollector
+
+__all__ = [
+    "BaseUrlCollector",
+    "FranceInfoUrlCollector",
+    "LadepecheFrUrlCollector",
+    "SlateFrUrlCollector",
+    "TF1InfoUrlCollector",
+]
